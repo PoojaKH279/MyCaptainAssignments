06a4f7034f8ddcb711290853996c90586abbc5a2
PoojaKH279 <108049908+PoojaKH279@users.noreply.github.com>
2022-07-14 13:33:58 +0530

    Add files via upload

---
+++ positive list.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+print(list1)
+for i in list1:
+	if i<0:
+		list1.remove(i)
+		print(list1)
+		
+list2=[12,14,-95,3]
+print(list2)
+for i in list2:
+	if i<0:
+		list2.remove(i)
+		print(list2)
+	
